a8c3c32d197548d4190578bd925785cf63b5cc69
SCORPI0N <39867440+SCORPI0N1@users.noreply.github.com>
2021-04-30 19:39:30 +0900

    Update serializers.py

--- todo_main/serializers.py
+++ todo_main/serializers.py
@@ -1,14 +1,14 @@
-from .models import Todo_list, Comment
+from .models import TodoList, Comment
 from rest_framework import serializers
 
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Todo_list
+        model = TodoList
         fields = ('id', 'title', 'description','is_completed','created_at','updated_at')
 
 class TodoDetailSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Todo_list
+        model = TodoList
         fields = ('title', 'description', 'is_completed', 'updated_at')
 
 class CommentSerializer(serializers.HyperlinkedModelSerializer):
