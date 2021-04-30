f127d62b2d698f5769cecd7488da4adcb6d17c76
SCORPI0N <39867440+SCORPI0N1@users.noreply.github.com>
2021-04-30 19:40:04 +0900

    Update views.py

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,21 +3,21 @@ from django.views import generic
 from rest_framework import viewsets
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 
-from .models import Todo_list, Comment
+from .models import TodoList, Comment
 from .serializers import TodoSerializer, TodoDetailSerializer, CommentSerializer
 
 
 class TodoIndex(viewsets.ModelViewSet):
-    queryset = Todo_list.objects.all()
+    queryset = TodoList.objects.all()
     serializer_class = TodoSerializer
 
 class TodoMain(ListAPIView):
-    queryset = Todo_list.objects.all().order_by('-created_at')
+    queryset = TodoList.objects.all().order_by('-created_at')
     serializer_class = TodoSerializer
 
 class TodoDetail(RetrieveAPIView):
     lookup_field = 'id'
-    queryset = Todo_list.objects.all()
+    queryset = TodoList.objects.all()
     serializer_class = TodoDetailSerializer
 
 class CommentIndex(viewsets.ModelViewSet):
